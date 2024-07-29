f2443571524060b90ae4e70711350e7156a2533a
MaylaSilva <may.siqueira93@gmail.com>
2024-07-29 14:32:21 -0300

    finalização de exercícios

--- Desafio_01/Exercicio_07/Ex_07.py
+++ Desafio_01/Exercicio_07/Ex_07.py
@@ -1,3 +1,11 @@
 '''Solicite ao usuário o peso em kg e a altura em metros. Calcule e
 imprima o Índice de Massa Corporal (IMC) usando a fórmula:
 IMC = peso / (altura x altura).'''
+
+print("Calculadora de IMC")
+
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura, em metros (exemplo: 1.6): "))
+imc=peso/(altura*altura)
+
+print(f"Seu IMC é de {imc:.2f}")
--- Desafio_01/Exercicio_10/Ex_10.py
+++ Desafio_01/Exercicio_10/Ex_10.py
@@ -3,3 +3,10 @@ uma mensagem amigável utilizando as variáveis criadas.
 Exemplos de variáveis: nome, idade, lugar, profissão ....
 Exemplo de retorno: Olá Maria, prazer te conhecer. Sou de São Paulo
 também e estou migrando de área'''
+
+nome=input("Qual é seu nome? ")
+hobby=input("Qual seu hobby favorito? ")
+linguagem=input("Qual linguagem você tem mais curiosidade para conhecer? ")
+tempoEstudo=input("Há quanto tempo está estudando programação? ")
+
+print(f"Oi {nome}, espero que esteja bem! {hobby}, parece muito divertido! Nossa, tem estudado {tempoEstudo} de programação? Espero que possa encaixar para aprender {linguagem}")
